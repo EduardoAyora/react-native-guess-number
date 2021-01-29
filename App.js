3d6ec82263624732e84609f23d61cf6d2ccd7feb
EduardoAyora <eduardoaayora64@gmail.com>
2021-01-29 12:06:15 -0500

    modal

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-  
-
   return (
     <View style={styles.screen}>
-      <Text>Hola</Text>
+      <Header title="Guess a Number" />
+      <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50
+    flex: 1
   }
 });
